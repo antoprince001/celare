cc39178b45cf75c00db71251a9339e67f0a24b09
antoprince001 <antoprince001@gmail.com>
2025-04-06 16:27:40 +0530

    Add support for selecting anonymization type in UI

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,14 @@
 // Function to anonymize content
-async function cloakContent(text) {
-
+async function cloakContent(text,anonymizationType) {
 
     let headersList = {
         "Content-Type": "application/json"
     }
-       
     let bodyContent = JSON.stringify({
-        "content" : text
+        "content" : text,
+        "type_of_anonymization" : anonymizationType
     });
-       
+
     let response = await fetch("http://localhost:8000/v1/anonymize", { 
         method: "POST",
         body: bodyContent,
@@ -23,11 +22,16 @@ async function cloakContent(text) {
 // Handle cloak button click
 document.getElementById("cloakButton").addEventListener("click", async () => {
     const inputText = document.getElementById("inputText").value;
+    const errorMsg = document.getElementById("errorMsg");
+    const outputText = document.getElementById("outputText");
+    const anonymizationType = document.getElementById("anonymizationType").value;
+    let errorMessageContent = "";
+    
     console.log("Working on cloaking content...");
-    document.getElementById("outputText").value = "";
+    outputText.value = "";
     errorMsg.textContent = "";
     try {
-        const cloakedData = await cloakContent(inputText);
+        const cloakedData = await cloakContent(inputText,anonymizationType);
         if (cloakedData === undefined || cloakedData === null) {
             errorMessageContent = "No response from server";
             errorMsg.textContent = errorMessageContent;
@@ -35,7 +39,7 @@ document.getElementById("cloakButton").addEventListener("click", async () => {
         }
         if('anonymized_text' in cloakedData){
             cloakedText = cloakedData['anonymized_text'];
-            document.getElementById("outputText").value = cloakedText;
+            outputText.value = cloakedText;
         }
         else if('error' in cloakedData){            
             errorMessageContent = cloakedData['error'];
@@ -54,8 +58,6 @@ document.getElementById("cloakButton").addEventListener("click", async () => {
 // Handle copy button click
 document.getElementById("copyButton").addEventListener("click", () => {
     const outputText = document.getElementById("outputText");
-    // outputText.select();
-    // document.execCommand("copy");
     navigator.clipboard.writeText(outputText.value).then(() => {
         alert("Cloaked content copied to clipboard!");
     }).catch(err => {
